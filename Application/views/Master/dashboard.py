fa7483e63bfaa60dd82d6c0ad49a77a0a2d870f1
hkbossdeveloper <xodeac-cc@outlook.com>
2020-07-01 14:43:10 +0500

    login Systems correction

--- Application/views/Master/dashboard.py
+++ Application/views/Master/dashboard.py
@@ -3,9 +3,6 @@ from flask import render_template , url_for , request ,jsonify, session , flash 
 from pymongo import MongoClient
 import json
 
-client = MongoClient("mongodb+srv://GCU:03162400202@cluster0-rczww.gcp.mongodb.net/admin?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
-db = client.GlobalCRM
-
 
 
 
